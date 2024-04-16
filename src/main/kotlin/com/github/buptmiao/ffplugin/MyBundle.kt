014e551c43afe2f38653ccb8f53f1f22b54d4fe5
GitHub Action <action@github.com>
2024-04-16 14:58:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/buptmiao/ffplugin/MyBundle.kt
+++ src/main/kotlin/com/github/buptmiao/ffplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.buptmiao.ffplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/buptmiao/ffplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/buptmiao/ffplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.buptmiao.ffplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
